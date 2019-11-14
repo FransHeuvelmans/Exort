1ed7cdbc0c617b87a84759063743778a0f425f7d
Frans van den Heuvel <f.vandenheuvel@qimia.de>
2019-11-14 19:46:08 +0100

    VaryRow sorting test

--- src/test/scala/dev/hillman/exort/InSortTest.scala
+++ src/test/scala/dev/hillman/exort/InSortTest.scala
@@ -50,4 +50,29 @@ class InSortTest extends FlatSpec with Matchers {
     reverseOut(2).v === 0.5
     reverseOut.last.v === -0.1
   }
+
+
+  "unsorted dev.hillman.Exort.VaryRow table after sorting" should "be sorted" in {
+    val sortOrder = Tools.sortKeyType.stringKeyType :: Tools.sortKeyType.integerKeyType :: Tools.sortKeyType.decimalKeyType :: Nil
+    val unsorted =
+      VaryRow("c" :: Nil, 5.0 :: Nil, 11 :: Nil, sortOrder, Array("c", "11", "5.0")) ::
+        VaryRow("d" :: Nil, 9.0 :: Nil, 13 :: Nil, sortOrder, Array("d", "13", "9.0")) ::
+        VaryRow("c" :: Nil, 4.0 :: Nil, 11 :: Nil, sortOrder, Array("c", "11", "4.0")) ::
+        VaryRow("e" :: Nil, 3.5 :: Nil, 15 :: Nil, sortOrder, Array("e", "15", "3.5")) ::
+        VaryRow("b" :: Nil, 2.0 :: Nil, 6 :: Nil, sortOrder, Array("b", "6", "2.0")) ::
+        VaryRow("a" :: Nil, 3.0 :: Nil, 2 :: Nil, sortOrder, Array("a", "2", "3.0")) ::
+        VaryRow("c" :: Nil, 3.0 :: Nil, 9 :: Nil, sortOrder, Array("c", "9", "3.0")) ::
+        VaryRow("a" :: Nil, 1.0 :: Nil, 1 :: Nil, sortOrder, Array("a", "1", "1.0")) :: Nil
+    val out = InSort.sortVaryRow(unsorted)
+    out(0).content(1) === "1"
+    out(1).content(1) === "2"
+    out(2).content(0) === "b"
+    out(3).content(0) === "c"
+    out(3).content(1) === "9"
+    out(4).content(1) === "11"
+    out(4).content(2) === "4.0"
+    out(5).content(2) === "5.0"
+    out(5).content(0) === "d"
+    out(6).content(0) === "e"
+  }
 }
